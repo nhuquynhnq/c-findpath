796b097df3f398c06a35c83d2aed6c2139ac839c
Nhu <nhuquynh.nqq@gmail.com>
2019-12-24 23:50:20 +0700

    first

---
+++ Demo/Demo/Demo/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawCheck = new System.Windows.Forms.CheckBox();
+            this.checkLine = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbfrom = new System.Windows.Forms.ComboBox();
+            this.cbto = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtdodai = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkDelete = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawCheck
+            // 
+            this.drawCheck.AutoSize = true;
+            this.drawCheck.Location = new System.Drawing.Point(9, 31);
+            this.drawCheck.Name = "drawCheck";
+            this.drawCheck.Size = new System.Drawing.Size(80, 17);
+            this.drawCheck.TabIndex = 0;
+            this.drawCheck.Text = "Draw Node";
+            this.drawCheck.UseVisualStyleBackColor = true;
+            // 
+            // checkLine
+            // 
+            this.checkLine.AutoSize = true;
+            this.checkLine.Location = new System.Drawing.Point(92, 31);
+            this.checkLine.Name = "checkLine";
+            this.checkLine.Size = new System.Drawing.Size(79, 17);
+            this.checkLine.TabIndex = 2;
+            this.checkLine.Text = "Draw Lines";
+            this.checkLine.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add weight:";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(72, 66);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(121, 20);
+            this.txtWeight.TabIndex = 4;
+            this.txtWeight.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.txtWeight.TextChanged += new System.EventHandler(this.txtWeight_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Find Path:";
+            // 
+            // cbfrom
+            // 
+            this.cbfrom.FormattingEnabled = true;
+            this.cbfrom.Location = new System.Drawing.Point(72, 113);
+            this.cbfrom.Name = "cbfrom";
+            this.cbfrom.Size = new System.Drawing.Size(121, 21);
+            this.cbfrom.TabIndex = 6;
+            // 
+            // cbto
+            // 
+            this.cbto.FormattingEnabled = true;
+            this.cbto.Location = new System.Drawing.Point(72, 148);
+            this.cbto.Name = "cbto";
+            this.cbto.Size = new System.Drawing.Size(121, 21);
+            this.cbto.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "To:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total:";
+            // 
+            // txtdodai
+            // 
+            this.txtdodai.Location = new System.Drawing.Point(63, 247);
+            this.txtdodai.Name = "txtdodai";
+            this.txtdodai.Size = new System.Drawing.Size(121, 20);
+            this.txtdodai.TabIndex = 11;
+            this.txtdodai.TextChanged += new System.EventHandler(this.txtdodai_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Path:";
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(63, 221);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(121, 20);
+            this.txtkq.TabIndex = 13;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(88, 182);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 14;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(234, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 16;
+            // 
+            // checkDelete
+            // 
+            this.checkDelete.AutoSize = true;
+            this.checkDelete.Location = new System.Drawing.Point(177, 31);
+            this.checkDelete.Name = "checkDelete";
+            this.checkDelete.Size = new System.Drawing.Size(57, 17);
+            this.checkDelete.TabIndex = 17;
+            this.checkDelete.Text = "Delete";
+            this.checkDelete.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.listbox);
+            this.groupBox1.Controls.Add(this.drawCheck);
+            this.groupBox1.Controls.Add(this.checkDelete);
+            this.groupBox1.Controls.Add(this.txtdodai);
+            this.groupBox1.Controls.Add(this.txtkq);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnFind);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.checkLine);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtWeight);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbto);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbfrom);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 449);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Show matrix";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(9, 312);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(257, 121);
+            this.listbox.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Name = "Form1";
+            this.Text = "Find Shortest Path";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox drawCheck;
+        private System.Windows.Forms.CheckBox checkLine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbfrom;
+        private System.Windows.Forms.ComboBox cbto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtdodai;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listbox;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Demo/Demo/Demo/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        int[,] matr = new int[100, 100];
+        Graphics g; Pen p; Point cursor;
+        int k =0, ki, jk; 
+        Point p1, p2;
+        string tmp = "0";
+        Point[] points = new Point[50];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            p = new Pen(Color.Black, 3);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void txtdodai_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            var text1 = k.ToString();
+            List<Point> p1List = new List<Point>();
+            List<Point> p2List = new List<Point>();
+            if (drawCheck.Checked == true && checkLine.Checked == false && checkDelete.Checked == false)
+
+            {
+                float emSize = 10;
+                //Ve cac node
+                g.DrawEllipse(p, cursor.X - 10, cursor.Y - 10, 20, 20);
+                SolidBrush b1 = new SolidBrush(Color.CornflowerBlue);
+                //To mau node
+                g.FillEllipse(b1, cursor.X - 10, cursor.Y - 10, 20, 20);
+                points[k++] = new Point(cursor.X, cursor.Y);
+               
+                //Đánh số cho các node
+                g.DrawString(text1, new Font(FontFamily.GenericSansSerif, emSize, FontStyle.Regular),
+                new SolidBrush(Color.Black), cursor.X + 10, cursor.Y + 10);
+                cbfrom.Items.Add(text1);
+                cbto.Items.Add(text1);
+
+
+
+            }
+            else if (drawCheck.Checked == false && checkLine.Checked == true && checkDelete.Checked == false)
+            {
+               
+                //Lấy vị trí các node cần vẽ đường nối
+                if (p1.X == 0)
+                {
+                    p1.X = cursor.X;
+                    p1.Y = cursor.Y;
+                    
+                }
+                else
+                {
+                    p2.X = cursor.X;
+                    p2.Y = cursor.Y;
+                    p1List.Add(p1);
+                    p2List.Add(p2);
+                    p1.X = 0;
+                    
+                }
+                using (var p = new Pen(Color.LemonChiffon, 2))
+                {
+                    for (int x = 0; x < p1List.Count; x++)
+                    {
+                        //vẽ đường nối
+                        g.DrawLine(p, p1List[x], p2List[x]);
+                        var ptMed = new Point((p1List[x].X + p2List[x].X) / 2, (p1List[x].Y + p2List[x].Y) / 2);
+                        var offset = g.MeasureString(tmp, this.Font);
+                        ptMed.Y -= (int)offset.Height;
+                        ptMed.X -= (int)offset.Width;
+                        //Đánh trọng lượng cho đường
+                        
+                        g.DrawString(tmp, this.Font, Brushes.Chocolate, ptMed);
+                        int imin = 20;
+                        int iminn = 20;
+                       
+                        for (int ii = 0; ii < k; ii++)
+                        {
+                            double kc = Math.Sqrt(Math.Abs((points[ii].X - p1List[0].X)) * Math.Abs((points[ii].X - p1List[0].X)) + Math.Abs((points[ii].Y - p1List[0].Y)) * Math.Abs((points[ii].Y - p1List[0].Y)));
+                            double kcc = Math.Sqrt(Math.Pow(Math.Abs((points[ii].X - p2List[0].X)), 2) + Math.Pow(Math.Abs((points[ii].Y - p2List[0].Y)), 2));
+                           
+                                if (kc < imin)
+                                {
+                                    ki = ii;
+                                }
+                            if (kcc < iminn)
+                            {
+                                jk = ii;
+                            }
+                        }
+                        matr[ki, jk] = int.Parse(tmp);
+                        matr[jk, ki] = int.Parse(tmp);
+                    }
+
+                }
+                
+            }
+            else if(checkDelete.Checked==true && checkLine.Checked==false && drawCheck.Checked == false)
+            {
+                this.Invalidate();
+                for(int i = 0; i < k; i++)
+                {
+                    points[i].X = points[i].Y = 0;
+                    
+                }
+                k = 0;
+                cbfrom.Items.Clear();
+                cbto.Items.Clear();
+                txtWeight.Text = "";
+                txtkq.Text = "";
+                txtdodai.Text = "";
+                listbox.Items.Clear();
+            }
+            
+        }
+
+
+
+
+
+
+       
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox objTextBox = (TextBox)sender;
+            tmp = objTextBox.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tmpp = "";
+            for (int j = 0; j < k; j++)
+            {
+                for (int v = 0; v < k; v++)
+
+                {
+                    tmpp += matr[j, v] + " ";
+
+
+                }
+                listbox.Items.Add(tmpp);
+                tmpp = "";
+            }
+        }
+
+        private void txtWeight_TextChanged(object sender, EventArgs e)
+        {
+            tmp = txtWeight.Text;
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+          
+            int d = int.Parse(cbfrom.SelectedItem.ToString());
+            int c = int.Parse(cbto.SelectedItem.ToString());
+            Dijkstra(matr, k, d, c);
+            if (txtkq.Text.Contains(c.ToString())==false)
+            {
+                txtkq.Text += c +" ";
+            }
+        }
+        private void Dijkstra(int[,] matr, int k, int d, int c)
+        {
+            int[] S = new int[100];
+            int[] Len = new int[100];
+            int []P = new int[100];
+            int  sum = 0;
+            int i ;
+          
+            // nhap ma tran va tinh gia tri vo cung (sum)
+            for (i = 0; i < k; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+
+                    sum += matr[i, j];
+                }
+            }
+            // dat vo cung cho tat ca cap canh khong noi voi nhau
+            for (int t = 0; t < k; t++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if (t!=j && matr[t,j] == 0)
+                        matr[t,j] = sum;
+                }
+            }
+
+            for (int j = 0; j < k; j++)
+            {
+                if (j != d)
+                {
+                    Len[j] = sum;                   // khoi tao do dai tu a toi moi dinh la vo cung
+                    S[j] = 0;                       // danh sach cac diem da xet
+                    P[j] = d;
+                }// dat diem bat dau cua moi diem la a
+            }
+
+            Len[d] = 0;                         // dat do dai tu a -> a la 0
+
+            // tim duong di ngan nhat tu 1 dinh den moi dinh khac thi thay bang vong for:
+            //for (int k = 0; k < n; k++)
+
+            while (S[c] == 0)
+            {                 // trong khi diem cuoi chua duoc xet
+                for (i = 0; i < k; i++)          // tim 1 vi tri ma khong phai la vo cung
+                    if (S[i]==0 && Len[i] < sum)
+                        break;
+
+                // i >=n tuc la duyet het cac dinh ma khong the tim thay dinh b -> thoat
+                if (i >= k)
+                {
+
+                    break;
+                }
+
+                for (int j = 0; j < k; j++)
+                {    // tim diem co vi tri ma do dai la min
+                    if (S[j]==0 && Len[i] > Len[j])
+                    {
+                        i = j;
+                    }
+                }
+
+                S[i] = 1;                       // cho i vao danh sach xet roi
+
+                for (int j = 0; j < k; j++)
+                {    // tinh lai do dai cua cac diem chua xet
+                    if (S[j]==0 && Len[i] + matr[i,j] < Len[j])
+                    {
+                        Len[j] = Len[i] + matr[i,j];      // thay doi len
+                        P[j] = i;                       // danh dau diem truoc no
+                    }
+                }
+            }
+
+                txtdodai.Text =Len[c].ToString();
+                if (Len[c] > 0 && Len[c] < sum)
+                {
+                // truy vet
+               
+                while (i!=d)
+                    {
+                    txtkq.Text += i.ToString() + "<==";
+                    i = P[i];
+                    }
+                txtkq.Text += d.ToString()+" ";
+
+            }
+            else
+                {
+                txtkq.Text = "khong co duong di tu "+d+" "+"sang "+c;
+                
+                }
+            
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Lấy vị trí
+            cursor = this.PointToClient(Cursor.Position);
+
+        }
+        
+
+
+    }
+}
